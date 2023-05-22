45ece0ee0fb0a409964b567e43c86e824395377c
JoseParra28 <iliftandcook@gmail.com>
2023-05-22 06:14:17 +0000

    ave

---
+++ test.js
@@ -0,0 +1,14 @@
+document.write("<h1>Hello there</h1>");
+document.write("<hr>");
+document.write("<p>This is my first great test</p>")
+
+let names = "Jose";
+let job = "Programmer";
+
+// document.write(names,"is a", job);
+
+document.write(100 + Number("25") + "<br>");
+document.write(100 + parseInt("50")+ "<br>");
+document.write(100 + parseFloat("50.99")+ "<br>");
+
+document.write("<button>Click here</button>");
