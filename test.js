9a30df2bc74231755d6b1e4ebeb6862f93ee2e66
JoseParra28 <iliftandcook@gmail.com>
2023-05-24 04:22:47 +0000

    save

--- test.js
+++ test.js
@@ -2,19 +2,43 @@ document.write("<h1>Hello there</h1>");
 document.write("<hr>");
 document.write("<p>This is my first great test</p>")
 
-let names = "Jose";
-let job = "Programmer";
+// let names = "Jose";
+// let job = "Programmer";
 
-const greeting = "Hello";
-const num = 100;
-
-document.getElementById("#btn");
+// const greeting = "Hello";
+// const num = 100;
 
 
+// document.write(10 ** 2,  "<br>");
+// document.write(greeting + " ", names + " ", job  + " ", Number(num / 2 ),   "<br>");
 
+// let user = window.prompt("Enter you name ");
+// alert("Your name is " + user)
 
-document.write(num + 10 * 2,  "<br>");
-document.write(greeting + " ", names + " ", job  + " ", Number(num / 2 ),   "<br>");
+let header = document.getElementById("myHeader");
+
+header.style="color: black; background-color: grey"
+document.write(header.getAttribute("giraffe"));
+header.innerHTML = "Elephant academy";
+
+//  errays
+
+const luckyNums = [4, 8, 15, 16, "twenty", false];
+
+luckyNums[0] = 90;
+document.write(luckyNums.length + "<br>")
+
+// objects\
+
+let student = {
+    usernmame: "Jose",
+    age: 29,
+    sector: "Programming"
+}
+student.usernmame = "Miguel"
+document.write(student.usernmame + "<br>")
+document.write(student.age + "<br>")
+document.write(student.sector + "<br>")
 
 
 
