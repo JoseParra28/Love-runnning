247b4fe7aceafa62c827cfb70ea9510ced3c8fab
JoseParra28 <iliftandcook@gmail.com>
2023-05-27 21:23:17 +0000

    save

--- test.js
+++ test.js
@@ -70,4 +70,116 @@ x *= 4;
 
 document.write(x + "<br>")
 
+// let theNumber = Number(prompt("Pick a number"));
+// if (!Number.isNaN(theNumber)){
+//     console.log("Your number in on the squere foot of " + " " + theNumber * theNumber); 
+// } else {
+//     console.log("Why the fuck did you not write a number you stupic slut!"); 
+// }
+
+// let test = (prompt("Pick a number"));
+// if (test < 10) {
+//     console.log("Small")
+// }else if (num < 100){
+//     console.log("Medium")
+// } else {
+//     console.log("big")
+// }
+
+// ----------------------------- If statement
+
+// let statement = (prompt("Guess my age"))
+// if (statement > 29){
+//     console.log("What the actual fuck!! I'm not that old!!")
+// } else if (statement == 29 ){
+//     console.log("Ye! That's correct")
+// }
+// else {
+//     console.log("Unfortunately I'm a little older than that")
+// } 
+
+// ----------------------------- While llop
+// let number = 0;
+// while (number <= 12){
+//     console.log(number);
+//     number = number + 1;
+// }
+
+// ----------------------------- While loop 2
+// let result = 1;
+// let counter = 0;
+// while (counter < 10){
+// result = result * 2;
+// counter = counter + 1;
+// }
+// console.log(result)
+
+// ----------------------------- Do loop
+// let yourName; 
+// do {
+//     yourName = (prompt("whats your name"));
+// } while (!yourName);
+// console.log(yourName )
+
+// ----------------------------- for loop
+// for(let number2 = 0; number2 <= 12; number2 = number2 + 1){
+//     console.log(number2)
+// }
+
+// ----------------------------- for loop with break
+
+// for(let current = 20; ; current = current = 1) {
+//     if (current % 7 == 0 ){
+//         console.log(current);
+//         break;
+//     }
+// }
+
+// let fruits = (prompt("Enter how many apples you have"));
+// let outcome;
+
+// if (fruits > 5 ){
+//     console.log("I can make apple juice")
+// }else if (fruits < 5) {
+//     console.log("I can't make apple juice")
+// }else if(fruits <= 5){
+//     console.log("I can make just enough apple juice")
+// }else {
+//     console.log("Apple juice is good")
+// }
+
+// --------------------------------------------- more if statement 
+
+// let days = (prompt("How many days are in one year"))
+
+// if (days < 365 ){
+//     document.write("There are more days in a year" + "<br>")
+// }else if (days == 366 ){
+//     document.write("Thats actually correct but every four years" + "<br>")
+// }else if (days > 366 ){
+//     document.write("There are less days in a year" + "<br>")
+// }else {
+//     document.write("Thats correct well done!" + "<br>")
+// }
+
+// --------------------------------------------- nested if / else
+
+// let id = (prompt("Plase verify you email"));
+// let verified = false;
+
+// if (id) {
+//     if (verified){
+//         document.write("Welcome" + "<br>")
+//     } else {
+//         document.write("You need to  verify your id" + "<br>")
+//     }
+// } else {
+//     document.write("you need to be a user to very your id" + "<br>")
+// }
+
+// --------------------------------------------- Errays
+// let clothes = ["top", "jeans", "shoes", "hat", "tie", "boxers"] 
+
+// let clotehsVar = clothes[0]
+// document.write(clotehsVar + "<br>")
 
