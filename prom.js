0bea371fa638f8c0e76ff3113f6517a3e37146b0
JoseParra28 <iliftandcook@gmail.com>
2023-06-03 04:36:48 +0000

    save

---
+++ prom.js
@@ -0,0 +1,26 @@
+
+// function myFunc(){
+//     document.getElementById("text").innerHTML = "Hello puta"
+//     document.getElementById("text").style.fontSize = "100px"
+//     document.getElementById("text").style.color = "#bd6b22"
+// };
+
+// document.getElementById("btn").addEventListener("click", function(){
+//     document.getElementById("p").innerHTML = "rakhbur";
+// });
+
+// let question = (prompt("What is 2 + 2"))
+
+// if (question < 4 ){
+//     document.getElementById("p").innerHTML = "no that is not correct";
+// }else if (question > 4 ){
+//     document.getElementById("p").innerHTML = "no that is ALSO not correct";
+// }else{
+//     document.getElementById("p").innerHTML = "well done";
+// }
+
+let x = 5;
+let y = 5;
+let sum = x + y;
+
+document.getElementById("text").innerHTML = sum;
